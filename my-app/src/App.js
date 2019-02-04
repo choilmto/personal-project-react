10415e2082065d767e544aef1048728dc61d2350
choilm <choilmto@gmail.com>
2019-02-03 21:19:51 -0500

    write github destructure functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//static info
+import { githubEvents } from './hw-events.js';
+import { pullRequest } from './hw-pull.js';
+
+const destructureForkEvents = (eventArr) => eventArr.filter(
+  (element) => element.type === "ForkEvent").map(
+  (element) => ({
+    id: element.id,
+    repoName: element.repo.name,
+    baseURL: element.repo.url,
+    created_at: element.created_at
+  })).sort((a, b) => Date(a.created_at) - Date(b.created_at));
+const destructurePullRequestEvents = (eventArr) => eventArr.filter(
+  (element) => element.type === "PullRequestEvent").map(
+  (element) => ({
+    id: element.id,
+    title: element.payload.pull_request.title,
+    link: element.payload.pull_request.html_url,
+    status: element.payload.action,
+    created_at: element.created_at
+  })).sort((a, b) => Date(a.created_at) - Date(b.created_at));
+
+console.log(destructurePullRequestEvents(githubEvents));
 
 class App extends Component {
   render() {
